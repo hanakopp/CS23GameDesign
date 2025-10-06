5bcad2eb14998b9b1ef9e8a79a4967a4b27e9b9c
jbeaniedutemple <jbdutemple29@gmail.com>
2025-10-06 11:05:11 -0400

    Added some assets

--- Assets/Scripts/PlayerMoveAimShoot.cs
+++ Assets/Scripts/PlayerMoveAimShoot.cs
@@ -4,67 +4,97 @@ using UnityEngine;
 
 public class PlayerMoveAimShoot : MonoBehaviour{
 
-      //public Animator animator;
-      public float moveSpeed = 5f;
+      
+
+
+
+
+      public Animator animator;
+      // public float moveSpeed = 5f;
       private Rigidbody2D rb;
-      public Camera cam;
-      public Vector2 movement;
-      public Vector2 mousePos;
-      public Transform fireBase;
-      public Transform firePoint;
+
+      public Transform ProjSpawn;
+      // public Camera cam;
+      // public Vector2 movement;
+      // public Vector2 mousePos;
+      // public Transform fireBase;
+      // public Transform firePoint;
+      public SpriteRenderer playerSprite;
      
       public GameObject projectilePrefab;
-      public float projectileSpeed = 10f;
+      // public float projectileSpeed = 10f;
       public float attackRate = 2f;
       private float nextAttackTime = 0f;
-      //public GameObject muzzleFlash;
 
-      void Start(){
-           //animator = gameObject.GetComponentInChildren<Animator>();
-           rb = GetComponent<Rigidbody2D>();
-           cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>() as Camera;
+      private Rigidbody2D projmass;
+      // //public GameObject muzzleFlash;
+      public Vector2 momentum;
+
+      void Start() {
+            animator = gameObject.GetComponentInChildren<Animator>();
+            rb = GetComponent<Rigidbody2D>();
+      
+
+      //      cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>() as Camera;
       }
+      void Update() {
 
-      void Update(){
-           movement.x = Input.GetAxisRaw("Horizontal");
-           movement.y = Input.GetAxisRaw("Vertical");
-           mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
+      //      movement.x = Input.GetAxisRaw("Horizontal");
+      //      movement.y = Input.GetAxisRaw("Vertical");
+      //      mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
            if (Time.time >= nextAttackTime){
                   //if (Input.GetKeyDown(KeyCode.Space))
                  if (Input.GetAxis("Attack") > 0){
-                        playerFire();
+                        PlayerFire();
                         nextAttackTime = Time.time + 1f / attackRate;
                   }
             }
       }
 
 
-      void FixedUpdate(){
-            //actual movement uses Rigidbody2D, so goes in FixedUpdate:
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+      // void FixedUpdate(){
+      //       //actual movement uses Rigidbody2D, so goes in FixedUpdate:
+      //       rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-            Vector2 lookDir = mousePos - rb.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-            //rb.rotation = angle;
-            fireBase.transform.eulerAngles = new Vector3(0, 0, angle);
+      //       Vector2 lookDir = mousePos - rb.position;
+      //       float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+      //       //rb.rotation = angle;
+      //       fireBase.transform.eulerAngles = new Vector3(0, 0, angle);
+      // }
+
+      void PlayerFire() {
+
+            GameObject projectile = Instantiate(projectilePrefab, ProjSpawn);
+            projmass = projectile.GetComponent<Rigidbody2D>();
+
+
+
+
+            Vector2 force = 20 * (ProjSpawn.position);
+
+            projmass.AddForce(force, ForceMode2D.Impulse);
+            
       }
 
-      void playerFire(){
-            //animator.SetTrigger ("Fire");
-            Vector2 fwd = (firePoint.position - this.transform.position).normalized;
 
-            /*
-            //MUZZLEFLASH:
-            GameObject muzFlash = Instantiate(muzzleFlash, firePoint.position, Quaternion.identity);
-            Vector2 muzDirection = firePoint.position - transform.position;
-            Quaternion muzRotation = Quaternion.LookRotation(muzDirection, Vector3.up);
-            muzFlash.transform.rotation = muzRotation;
-            */
+      // void playerFire(){
+      //       //animator.SetTrigger ("Fire");
+      //       Vector2 fwd = (firePoint.position - this.transform.position).normalized;
 
-            //PROJECTILE
-            Quaternion onside = new Quaternion(90f, 0f, 0f, 0f);
-            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, onside);
-            projectile.GetComponent<Rigidbody2D>().AddForce(fwd * projectileSpeed, ForceMode2D.Impulse);
-      }
+      //       /*
+      //       //MUZZLEFLASH:
+      //       GameObject muzFlash = Instantiate(muzzleFlash, firePoint.position, Quaternion.identity);
+      //       Vector2 muzDirection = firePoint.position - transform.position;
+      //       Quaternion muzRotation = Quaternion.LookRotation(muzDirection, Vector3.up);
+      //       muzFlash.transform.rotation = muzRotation;
+      //       */
+
+      //       //PROJECTILE
+      //       Quaternion onside = new Quaternion(90f, 0f, 0f, 0f);
+      //       GameObject projectile = Instantiate(projectilePrefab, firePoint.position, onside);
+      //       projectile.GetComponent<Rigidbody2D>().AddForce(fwd * projectileSpeed, ForceMode2D.Impulse);
+      // }
 }
--- Assets/Scripts/ZombiePlayer.cs
+++ Assets/Scripts/ZombiePlayer.cs
@@ -19,7 +19,20 @@ public class ZombiePlayer : MonoBehaviour
 
     private bool facingRight;
 
+    public Transform FireBase;
 
+    public Vector2 momentum;
+    public Vector2 direction;
+
+    private Vector3 ProjDirection;
+
+    public Sprite UpSprite;
+    public Sprite DownSprite;
+    public Sprite SideSprite;
+
+    public GameObject projpre;
+
+    private Rigidbody2D projmass;
 
 
     // --- NEW ---
@@ -33,6 +46,7 @@ public class ZombiePlayer : MonoBehaviour
 
         rb.gravityScale = 0;
         rb.freezeRotation = true;
+        FireBase.position = Vector2.zero;
     }
 
     void Update()
@@ -42,7 +56,12 @@ public class ZombiePlayer : MonoBehaviour
         moveDirection.y = Input.GetAxisRaw("Vertical");
 
 
+        momentum = (moveDirection).normalized;
+
+
         moveDirection = moveDirection.normalized;
+
+
 
         if (animator != null)
         {
@@ -55,21 +74,54 @@ public class ZombiePlayer : MonoBehaviour
             animator.SetFloat("Y", lastMoveDirection.y);
         }
 
-        // if (moveDirection.x < 0) {
-        //     spriteRenderer.flipX = true;
-        // }
+        if (spriteRenderer != null && moveDirection.y > 0f)
+        {
+            spriteRenderer.sprite = UpSprite;
+            direction = Vector2.up;
+
+        }
+        if (spriteRenderer != null && moveDirection.y < 0f)
+        {
+            spriteRenderer.sprite = DownSprite;
+            direction = Vector2.down;
+        }
+
 
         if (spriteRenderer != null && moveDirection.x != 0f)
+        {
+            spriteRenderer.sprite = SideSprite;
             spriteRenderer.flipX = moveDirection.x < 0f;
+            if (spriteRenderer.flipX) { direction = Vector2.left; }
+            else { direction = Vector2.right ; }
+        }
 
+        if (moveDirection != Vector2.zero) {
+            Vector2 halfway = 0.5f * (direction).normalized;
+            FireBase.localPosition = halfway;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            PlayerFire();
+        }
         
 
-
+        
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = moveDirection * speed;
+
+        // FireBase.localPosition = momentum;
+
+        // if (spriteRenderer.sprite == DownSprite) {
+        //     FireBase.localPosition = Vector2.down;
+        // }
+        // else if (spriteRenderer.sprite == UpSprite) {
+        //     FireBase.localPosition = Vector2.up;
+        // } else if (spriteRenderer.sprite == SideSprite ) {
+        //     if (spriteRenderer.flipX) { FireBase.localPosition = Vector2.left; }
+        //     else { FireBase.localPosition = Vector2.right; }
+        // }
     }
 
     // --- NEW: flips the sprite upon moving left/right
@@ -82,5 +134,15 @@ public class ZombiePlayer : MonoBehaviour
     // }
 
     // --- NEW: call this when the player touches a powerup ---
+
+    void PlayerFire()
+    {
+        GameObject projectile = Instantiate(projpre, FireBase, false);
+        projectile.transform.parent = null;
+        projmass = projectile.GetComponent<Rigidbody2D>();
+        Vector2 force = 50 * direction;
+        projmass.AddForce(force, ForceMode2D.Impulse);
+
+    }
 
 }
